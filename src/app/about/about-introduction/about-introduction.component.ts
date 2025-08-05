35cf823321db17b087bf09d9cfd451c188cc342c
youssef_emad <youssefmeadhassan66@gmail.com>
2025-08-06 02:14:45 +0300

    Updated showcase images and about photo and home header h1

--- src/app/about/about-introduction/about-introduction.component.ts
+++ src/app/about/about-introduction/about-introduction.component.ts
@@ -15,6 +15,6 @@ export class AboutIntroductionComponent implements OnInit{
   ngOnInit(): void {
     this.Header = 'About me';
     this.description = 'Hello my name is Youssef Emad, a passionate Full-Stack Developer with a strong foundation in modern web technologies ,I honed my skills in various programming languages and developed a deep understanding of software development. My curiosity for solving complex problems led me to specialize in Full-Stack Development \n, I am  committed to turning ideas into reality by crafting seamless web experiences and leveraging data to drive decision-making. ';
-    this.image = './assets/images/Astronuat1.jpg';
+    this.image = './assets/images/youssefff.jpg';
   }
 }
--- src/app/show-cases/showcases-sections/showcases-sections.component.ts
+++ src/app/show-cases/showcases-sections/showcases-sections.component.ts
@@ -11,38 +11,29 @@ export class ShowcasesSectionsComponent implements OnInit{
       {
         "sectionTitle": "Exodia-OS Website:",
         "sectionDescription": "Exodia-OS is a customized Arch-based Linux distribution designed for cybersecurity experts. I played a key role in developing the website, focusing on creating a user-friendly interface that aligns with the technical nature of the OS.",
-         "sectionImage": {
-            "image" : "/assets/imagespexels-canvastudio-3153204.jpg"
-        },
+         "image" : "./assets/images/Showcase_1.png",
         "sectionLink": "https://github.com/Exodia-OS/exodia-os.github.io"
       },
 
       {
-        "sectionTitle": "E-commerce Store",
-        "sectionDescription": "This project is a application that fetches product data from a fake store API, displays products, allows filtering, pagination, and cart functionality like Product Display: Fetches product data from Fake Store API., Displays products in a card format with image, title, price, and buttons to add to cart or view details.",
-         "sectionImage": {
-            "image" : "./assets/imagespexels-canvastudio-3153204.jpg"
-        },
-        "sectionLink": "https://github.com/youssefemadhassan66/NTI-STORE-API"
+        "sectionTitle": "Revin Roams",
+        "sectionDescription": "TRiven Roams is a secure and dynamic web platform designed to streamline the tour booking experience. It offers features for both regular users and administrators, making it a comprehensive solution for managing and exploring tours. Features User Features",
+        "image" : "./assets/images/Showcase_3.png",
+
+        "sectionLink": "https://github.com/youssefemadhassan66/Riven-Roams-Tours-website-"
       },
 
       {
         "sectionTitle": "Chess Game ",
         "sectionDescription": "Made a chess game between 2 players using HTML , CSS , JaveScript made the board and the functionality for every piece to move on the board some another functions that are associated to chess rules like double moving for pawns and check for the king ",
-         "sectionImage": {
-            "image" : "/assets/imagespexels-canvastudio-3153204.jpg"
-
-        },
+         "image" : "./assets/images/Showcase_2.png",
         "sectionLink": "https://github.com/youssefemadhassan66/Chess-Game"
       },
 
       {
         "sectionTitle": " Steel-Express ",
         "sectionDescription": "An The Inventory Management System (IMS) integrates RFID, barcode scanning, and real-time analytics for comprehensive stock tracking and Automated triggers within the IMS initiate replenishment actions based on predefined thresholds, minimizing holding costs and preventing shortages. ",
-         "sectionImage": {
-            "image" : "/assets/imagespexels-canvastudio-3153204.jpg"
-
-        },
+         "image" : "./assets/images/Showcase_4.png",
         "sectionLink": "https://github.com/youssefemadhassan66/Steel-Express"
       }
     ];
